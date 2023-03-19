115fc13d429c3009a41e464dd7e4ef622d254b62
Valery <valera.vlasov11@mail.ru>
2023-03-19 18:38:42 +0300

    first version server

---
+++ cmd/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"github.com/ValeryVlasov/Smarthouse_server"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	if err := initConfig(); err != nil {
+		logrus.Fatalf("error initializing configs: %s", err.Error())
+	}
+	srv := new(Smarthouse_server.Server)
+	go func() {
+		if err := srv.Run(viper.GetString("port")); err != nil {
+			logrus.Fatalf("An error occured while starting server: %s", err.Error())
+		}
+	}()
+	logrus.Print("Server started")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	logrus.Print("Server stopped")
+
+	if err := srv.Shutdown(context.Background()); err != nil {
+		logrus.Errorf("An error occured while server shutting down: %s", err.Error())
+	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
+}
